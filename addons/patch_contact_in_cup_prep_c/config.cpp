1329bd6643b4bfa38f9f624ae8ee0c432c778563
Oliver Blanthorn <freedom4cows@gmail.com>
2019-09-05 14:56:18 +0100

    Attempt to debug why deletion wasn't working

--- addons/patch_contact_in_cup_prep_c/config.cpp
+++ addons/patch_contact_in_cup_prep_c/config.cpp
@@ -1,25 +1,35 @@
 #include "script_component.hpp"
-    class CfgPatches {
-        class ADDON {
-            name = COMPONENT;
-            units[] = {};
-            weapons[] = {};
-            requiredVersion = REQUIRED_VERSION;
-            requiredAddons[] = {
-                "CUP_Worlds",
-                "CUP_Church_01",
-                "CUP_CAStructuresHouse_Church_02",
-                "CUP_CAStructuresHouse_HouseV",
-                "CUP_CAStructuresHouse_HouseV2",
-                "CUP_Buildings2_Config",
-                "CUP_Structures_Config",
-                "CUP_New_Buildings"
-            };
-            author = "16AA 3LSR";
-            authors[] = {"Blanthorn","Dunwall"};
-            VERSION_CONFIG;
-        };
+class CfgPatches {
+    class CUP_Church_01 {
+        units[] = {};
     };
+    class Church_01 {
+        units[] = {};
+    };
+    class ADDON {
+        name = COMPONENT;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            /* "CUP_Worlds", */
+            "Church_01",
+            /* "CAStructuresHouse_Church_02", */
+            /* "CAStructuresHouse_HouseV", */
+            /* "CAStructuresHouse_HouseV2", */
+            /* "CUP_CAStructuresHouse_Church_02", */
+            /* "CUP_CAStructuresHouse_HouseV", */
+            /* "CUP_CAStructuresHouse_HouseV2", */
+            /* "CUP_Buildings2_Config", */
+            /* "CUP_Structures_Config", */
+            /* "CUP_New_Buildings", */
+            "CUP_Church_01",
+        };
+        author = "16AA 3LSR";
+        authors[] = {"Blanthorn","Dunwall"};
+        VERSION_CONFIG;
+    };
+};
 
 
 #include "CfgEventHandlers.hpp"
