9ce0ed6ffdca879178214d19954d8e6a4bb82ec3
Oliver Blanthorn <freedom4cows@gmail.com>
2019-10-13 13:15:02 +0100

    Replace more hay bales

--- addons/contact_in_cup_aesthetics_c/CfgVehicles.hpp
+++ addons/contact_in_cup_aesthetics_c/CfgVehicles.hpp
@@ -29,4 +29,64 @@ class CfgVehicles {
         {
         };
     };
+    class Misc_thing;
+    class Haystack_small {
+        author = "Bohemia Interactive";
+        mapSize = 1.58;
+        class SimpleObject
+        {
+            eden = 0;
+            animate[] = {};
+            hide[] = {};
+            verticalOffset = 0.672;
+            verticalOffsetWorld = 0;
+            init = "''";
+        };
+        editorPreview = "\A3\EditorPreviews_F_Enoch\Data\CfgVehicles\Land_HayBale_01_F.jpg";
+        _generalMacro = "Land_HayBale_01_F";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Hay Bale";
+        model = "\A3\Structures_F_Enoch\Industrial\Agriculture\HayBale_01_F.p3d";
+        DLC = "Enoch";
+        icon = "iconObject_1x2";
+        editorCategory = "EdCat_Structures_Enoch";
+        editorSubcategory = "EdSubcat_Agricultural";
+        vehicleClass = "Structures_Village";
+        destrType = "DestructTent";
+        cost = 1000;
+        class DestructionEffects
+        {
+        };
+    };
+    class House;
+    class Land_seno_balik {
+        author = "Bohemia Interactive";
+        mapSize = 1.58;
+        class SimpleObject
+        {
+            eden = 0;
+            animate[] = {};
+            hide[] = {};
+            verticalOffset = 0.672;
+            verticalOffsetWorld = 0;
+            init = "''";
+        };
+        editorPreview = "\A3\EditorPreviews_F_Enoch\Data\CfgVehicles\Land_HayBale_01_F.jpg";
+        _generalMacro = "Land_HayBale_01_F";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Hay Bale";
+        model = "\A3\Structures_F_Enoch\Industrial\Agriculture\HayBale_01_F.p3d";
+        DLC = "Enoch";
+        icon = "iconObject_1x2";
+        editorCategory = "EdCat_Structures_Enoch";
+        editorSubcategory = "EdSubcat_Agricultural";
+        vehicleClass = "Structures_Village";
+        destrType = "DestructTent";
+        cost = 1000;
+        class DestructionEffects
+        {
+        };
+    };
 };
