340cc1b9af9d5334875c467717d16a5abe4a3a56
Oliver Blanthorn <freedom4cows@gmail.com>
2019-10-20 17:20:49 +0100

    Add absolute paths to ReplaceObjects
    This appears to be necessary but it still doesn't work

--- addons/cfgworlds_replace_objects_c/ReplaceObjects.hpp
+++ addons/cfgworlds_replace_objects_c/ReplaceObjects.hpp
@@ -1,6 +1,36 @@
 class ReplaceObjects {
     class HaystackSwap {
-        replace = "stoh.p3d";
+        replace = "\ca\buildings\misc\stoh.p3d";
         with[] = {"\A3\Structures_F_Enoch\Industrial\Farms\StrawStack_01_F.p3d"};
+    };
+
+    class PiceaSwap {
+        replace = "\ca\plants2\tree\t_picea1s.p3d";
+        with[] = {"\a3\vegetation_f_enoch\tree\t_piceaabies_2s.p3d"};
+    };
+
+    class PopulusSwap {
+        replace = "\ca\plants2\tree\t_populus3s.p3d";
+        with[] = {"\A3\Vegetation_F_Enoch\Tree\t_PopulusN3s_F.p3d"};
+    };
+
+    class PyrusSwap {
+        replace = "\ca\plants2\tree\t_pyrus2s.p3d";
+        with[] = {"\A3\Vegetation_F_Enoch\Tree\t_pyrusCommunis_3s.p3d"};
+    };
+
+    class SalixSwap {
+        replace = "\ca\plants2\tree\t_salix2s.p3d";
+        with[] = {"\A3\Vegetation_F_Enoch\Tree\t_salix2s.p3d"};
+    };
+
+    class SorbusSwap {
+        replace = "\ca\plants2\tree\t_sorbus2s.p3d";
+        with[] = {"\A3\Vegetation_F_Enoch\Tree\t_sorbus2s.p3d"};
+    };
+
+    class PiceaStumpSwap {
+        replace = "\ca\plants2\tree\t_stub_picea.p3d";
+        with[] = {"\A3\Vegetation_F_Enoch\Tree\t_stub_picea.p3d"};
     };
 };
