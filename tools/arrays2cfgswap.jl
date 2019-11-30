4d73d0e658fb07baec987284d527419c59b5808a
Oliver Blanthorn <freedom4cows@gmail.com>
2019-11-30 12:45:51 +0000

    Start working on making parsing cleverer
    Fixing the destruction effects issue won't actually be fixed
    by this as we can't change inheritance of existing classes.
    
    I've given up as I think an AiO recursive dump will fix this
    more easily

--- tools/arrays2cfgswap.jl
+++ tools/arrays2cfgswap.jl
@@ -3,12 +3,13 @@
 # Usage: ./arrays2cfgwap.jl '["newname1", "newname2"]' '["oldname1", "oldname2"]'
 # NB: this doesn't currently deal with inheritance
 
-cd(@__DIR__)
-new = eval(Meta.parse(ARGS[1]))
-old = eval(Meta.parse(ARGS[2]))
+import JSON
 
+aio_json = JSON.parsefile("AiO.1.94.145977.json")["bin\\config.bin"] # Update when AiO is updated
 
 aio = readdir(".") |> f -> filter(x->occursin("AiO",x), f)[1] |> readlines
+
+fuzzyget(key,dict) = filter((k,v) -> occursin(key,k),dict)
 
 function classlines(classname)
     openbr = 0
@@ -28,23 +29,54 @@ function classlines(classname)
     lines
 end
 
-baseclasses = Set()
-for i in 1:length(old)
-    r = Regex("class $(old[i])[:]")
-    b = filter(x->occursin(r,x),aio)[1] |> x -> split(x,':')[end] # Doesn't deal with " : " but these don't exist in AiO
-    push!(baseclasses, b)
+function getbaseclasses!(classname, arr=[])
+    class = fuzzyget(Regex("^" * classname * ":"), aio_json["CfgVehicles"])
+    if (length(class) > 1) 
+        throw("Classname $(classname) ambiguous")
+    elseif (length(class) == 0)
+        return arr
+    end
+    baseclass = (class |> keys |> first |> x -> split(x,":"))[2] |> lstrip
+    push!(arr,baseclass)
+    getbaseclasses!(baseclass,arr)
+    arr
 end
 
-for b in baseclasses
-    println("class $b;")
+# get keys that need to be copied from parent class of new class
+function getparentkeysrequired(classname)
+    childkeys = fuzzyget(":",(fuzzyget(Regex("^" * classname * ":"),aio_json["CfgVehicles"]) |> values |> first)) |> keys
+    if length(childkeys) == 0
+        return []
+    end
+    childkeys .|> x -> split(x,":")[2] |> lstrip
 end
 
-for i in 1:length(old)
-    # yes, this is lazy
-    r = Regex("class $(old[i])[:]")
-    baseclass = filter(x->occursin(r,x),aio)[1] |> x -> split(x,':')[end]
-    println("class $(old[i]): $baseclass {")
-    for r in classlines(new[i])[3:end] # first line is classname, second is { on a newline in AiO
-        println(r)
+# This will just be easier with a recursive AiO dump of CfgVehicles
+
+if (length(ARGS) > 2)
+    cd(@__DIR__)
+    new = eval(Meta.parse(ARGS[1]))
+    old = eval(Meta.parse(ARGS[2]))
+
+
+    baseclasses = Set()
+    for i in 1:length(old)
+        r = Regex("class $(old[i])[:]")
+        b = filter(x->occursin(r,x),aio)[1] |> x -> split(x,':')[end] # Doesn't deal with " : " but these don't exist in AiO
+        #= push!(baseclasses, getbaseclasses!($(old[i]))...) =#
+    end
+
+    for b in baseclasses
+        println("class $b;")
+    end
+
+    for i in 1:length(old)
+        # yes, this is lazy
+        r = Regex("class $(old[i])[:]")
+        baseclass = filter(x->occursin(r,x),aio)[1] |> x -> split(x,':')[end]
+        println("class $(old[i]): $baseclass {")
+        for r in classlines(new[i])[3:end] # first line is classname, second is { on a newline in AiO
+            println(r)
+        end
     end
 end
